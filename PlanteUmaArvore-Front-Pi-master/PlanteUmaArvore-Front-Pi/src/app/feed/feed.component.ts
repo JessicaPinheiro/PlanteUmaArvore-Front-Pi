af27764b8a3ffab812ec281646a2360e00a740c7
Jéssica Pinheiro <69260930+JessicaPinheiro@users.noreply.github.com>
2020-10-07 15:53:23 -0300

    implementação do get-tema e get-postagem

--- PlanteUmaArvore-Front-Pi-master/PlanteUmaArvore-Front-Pi/src/app/feed/feed.component.ts
+++ PlanteUmaArvore-Front-Pi-master/PlanteUmaArvore-Front-Pi/src/app/feed/feed.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  key = 'data'
+  reverse = true
 
-  ngOnInit(): void {
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+  
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema : number
+  
+
+  constructor(private postagemService: PostagemService, private temaService : TemaService) { }
+
+  
+
+  ngOnInit() {
+
+    this.findAllPostagens()
+    this.findAllTemas()
+
   }
+
+  findAllPostagens() {
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp
+    })
+  }
+
+  findAllTemas() {
+    this.temaService.getAllTemas().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema() {
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) => {
+      this.tema = resp
+    })
+  }
+
 
 }
